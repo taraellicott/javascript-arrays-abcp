5c030bef4eff2621777aaa18926f333ad7cf94a0
Tara Ellicott <taraellicott@gmail.com>
2018-01-14 17:51:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+
+// function addElementToBeginningOfArray(arr,elem) {
+//   var spreadBeg = [elem,...arr];
+//   return spreadBeg;
+// }
+
+
+function destructivelyAddElementToBeginningOfArray(arr,elem) {
+  arr.unshift(elem);
+}
+
+
+// function addElementToEndOfArray(arr,elem) {
+//   ...arr,elem;
+// }
+
+
+function destructivelyAddElementToEndOfArray(arr,elem) {
+  arr.push(elem);
+}
+
+
+function accessElementInArray(arr,idx) {
+  return arr[idx];
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+}
+
+
+function removeElementFromBeginningOfArray(arr) {
+  arr.slice(0,1);
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+}
+
+
+function removeElementFromEndOfArray(arr) {
+  arr.slice(arr.length-1);
+}
+
