fc5b20a9632301c54175022065be409d82e0934a
Tara Ellicott <taraellicott@gmail.com>
2018-01-14 17:55:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr) {
 
 function removeElementFromBeginningOfArray(arr) {
   arr.slice(0,1);
+  return arr;
 }
 
 
@@ -47,5 +48,6 @@ function destructivelyRemoveElementFromEndOfArray(arr) {
 
 function removeElementFromEndOfArray(arr) {
   arr.slice(arr.length-1);
+  return arr;
 }
 
